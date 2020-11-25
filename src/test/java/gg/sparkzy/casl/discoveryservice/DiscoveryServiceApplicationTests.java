158dfd728a32540c57a06369e4cd3dfca8a2bc54
sparkzy <31968082+sparkzy@users.noreply.github.com>
2020-11-24 21:05:13 -0500

    First commit

---
+++ src/test/java/gg/sparkzy/casl/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.sparkzy.casl.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
